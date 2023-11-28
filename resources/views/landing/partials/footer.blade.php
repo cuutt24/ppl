dd243e5f639a616a4ce331aa173c719ddaef0533
DwiParzIbr <dwifiibrahim2@gmail.com>
2023-11-28 11:44:32 +0700

    first commit

---
+++ resources/views/landing/partials/footer.blade.php
@@ -0,0 +1,71 @@
+<footer class="text-white"
+    style="font-family: 'Epilogue', sans-serif;padding-left: 35px;padding-right: 35px;  background: rgb(6, 71, 8);">
+    <script src="https://unpkg.com/feather-icons"></script>
+    <div class="container py-4 py-lg-5">
+        <div class="row justify-content-center px-3">
+            <div
+                class="col-lg-6 col-sm-text-center text-lg-start d-flex flex-column align-items-center  align-items-lg-start">
+                <div class="fw-bold d-flex align-items-center mb-2">
+                    <a class="navbar-brand d-flex align-items-center" data-bss-hover-animate="pulse" href="/">
+                        <img src="{{ asset('storage/images/logo.png') }}" alt="Logo" width="230" height="48"
+                            class=""></a>
+                </div>
+                <p class="text-white" style="text-align: justify">Website Resmi Sumber Rejo, Kecamatan Hulu Palik, Kabupaten Bengkulu Utara.
+                    <br>Berfungsi sebagai pusat informasi, transparansi dan sosialisasi pemerintah
+                    bagi masyarakat selaku penerima manfaat.</p>
+            </div>
+            <div class="col-sm-4-text-center  col-md-3 text-lg-start d-flex flex-column">
+                <h3 class="fs-6 text-white">TAUTAN</h3>
+                <ul class="list-unstyled">
+                    <li data-bss-hover-animate="pulse">
+                        <a class="navbar-brand" href="https://bengkuluutarakab.go.id/" target="_blank">
+                            <i data-feather="globe" width="1.1em" height="1.1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"></i>
+                            Website Kabupaten Bengkulu Utara
+                        </a>
+                    </li>
+                    <li data-bss-hover-animate="pulse">
+                        <a class="navbar-brand" href="https://www.facebook.com//" target="_blank">
+                            <i data-feather="facebook" width="1.2em" height="1.2em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"></i>
+                            Facebook
+                        </a>
+                    </li>
+                    <li data-bss-hover-animate="pulse">
+                        <a class="navbar-brand" href="https://www.instagram.com/" target="_blank">
+                            <i data-feather="instagram" width="1.2em" height="1.2em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"></i>
+                            instagram
+                        </a>
+                    </li>
+                    <li data-bss-hover-animate="pulse">
+                        <a class="navbar-brand" href="https://twitter.com/" target="_blank">
+                            <i data-feather="twitter" width="1.2em" height="1.2em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"></i>
+                            Twitter
+                        </a>
+                    </li>
+                    <li data-bss-hover-animate="pulse">
+                        <a class="navbar-brand" href="https://www.youtube.com/" target="_blank">
+                            <i data-feather="youtube" width="1.2em" height="1.2em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"></i>
+                            Youtube
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-sm-4-text-center col-md-3 text-lg-start d-flex flex-column">
+                <h3 class="fs-6 text-white">KONTAK KAMI</h3>
+                <ul class="list-unstyled">
+                    <li>Pemerintahan Desa Sumber Rejo</li>
+                    <li>E-Mail: dfarizibrahim14@gmail.com</li>
+                    <li>Nomor Telp: 123456789 (24 Jam)</li>
+                </ul>
+            </div>
+
+        </div>
+        <hr />
+        <div class="d-flex justify-content-between align-items-center pt-3">
+            <p class="mb-0">Copyright © 2023 Pemerintahan Desa Sumber Rejo | Universitas
+                Bengkulu</p>
+        </div>
+    </div>
+    <script>
+        feather.replace();
+      </script>
+</footer>
